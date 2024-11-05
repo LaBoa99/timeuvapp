6273e0edf69eee3b6a2a9c5ef03946ec9f486482
Vladimir Ortiz <vladimir.ortiz8015@alumnos.udg.mx>
2024-11-05 13:49:18 -0600

    Carga inicial

--- app/src/main/java/cuvallesl/timeuv_app/MainActivity.kt
+++ app/src/main/java/cuvallesl/timeuv_app/MainActivity.kt
@@ -20,10 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TimeuvappTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
