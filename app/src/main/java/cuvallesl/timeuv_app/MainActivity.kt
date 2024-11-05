8580e806dd17e8431e5926e915d76ff1c4f46a81
Vladimir Ortiz <vladimir.ortiz8015@alumnos.udg.mx>
2024-11-05 13:51:51 -0600

    Carga inicial ventanas

--- app/src/main/java/cuvallesl/timeuv_app/MainActivity.kt
+++ app/src/main/java/cuvallesl/timeuv_app/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             TimeuvappTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Ventanas",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
