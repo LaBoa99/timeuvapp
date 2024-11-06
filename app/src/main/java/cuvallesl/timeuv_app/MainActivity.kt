7f218d19e1f6ebc1203649a2f37e4ea67c222037
Enrique Fregoso <enrique.fregoso4198@alumnos.udg.mx>
2024-11-06 11:48:50 -0600

    Version 1.0 del manejo y estructura de las ventanas a utilizar, servira como base para el completo desarrollo de las ventanas

--- app/src/main/java/cuvallesl/timeuv_app/MainActivity.kt
+++ app/src/main/java/cuvallesl/timeuv_app/MainActivity.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import cuvallesl.timeuv_app.ui.theme.TimeuvappTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +25,56 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            MainApp()
 
+        }
+    }
+}
+@Composable
+fun MainApp() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "first_window") {
+        composable("first_window") { FirstWindow(navController) }
+        composable("second_window/{name}") { backStackEntry ->
+            val name = backStackEntry.arguments?.getString("name")
+            SecondWindow(navController, name)
+        }
+    }
+}
+
+@Composable
+fun FirstWindow(navController: NavController) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Esta es la primera ventana")
+            Button(onClick = { navController.navigate("second_window/John") }) {
+                Text("Ir a la segunda ventana")
+            }
+        }
+    }
+}
+
+@Composable
+fun SecondWindow(navController: NavController, name: String?) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Esta es la segunda ventana")
+            name?.let { Text("Hola, $it!") }
+            Button(onClick = { navController.navigate("first_window") }) {
+                Text("Volver a la primera ventana")
+            }
         }
     }
 }
