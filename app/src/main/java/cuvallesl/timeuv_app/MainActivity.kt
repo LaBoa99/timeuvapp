745ea2f90d2ea859e8709fefcb49f6211cdf1d84
Enrique Fregoso <enrique.fregoso4198@alumnos.udg.mx>
2024-11-06 21:17:23 -0600

    Version 1.7 Ventanas Funcionales, pendientes a edicion algunas, desplazamiento por ventanas, listo

--- app/src/main/java/cuvallesl/timeuv_app/MainActivity.kt
+++ app/src/main/java/cuvallesl/timeuv_app/MainActivity.kt
@@ -1,60 +1,13 @@
 package cuvallesl.timeuv_app
 //Importacion de ventanas y procesos
-import cuvallesl.timeuv_app.views.*
 import cuvallesl.timeuv_app.navigation.NavManager
 //dependencias
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.TextButton
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.NavController
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.dp
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 
 
@@ -66,7 +19,7 @@ class MainActivity : ComponentActivity() {
             // Usar un NavController para la navegación
             val navController = rememberNavController()
             Surface(color = MaterialTheme.colorScheme.background) {
-                NavManager(navController = navController) // Configurar el NavGraph
+                NavManager(navController = navController) // Configurar el NavManager
             }
         }
     }
