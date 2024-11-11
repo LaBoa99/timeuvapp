53e24c962a694930d5afdf1cc35ea0f9090602ea
Enrique Fregoso <enrique.fregoso4198@alumnos.udg.mx>
2024-11-11 09:22:25 -0600

    Version 2.2

--- app/src/main/java/cuvallesl/timeuv_app/views/CalendarView.kt
+++ app/src/main/java/cuvallesl/timeuv_app/views/CalendarView.kt
@@ -39,7 +39,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 
-
+//Probando nuevo versionado
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 
