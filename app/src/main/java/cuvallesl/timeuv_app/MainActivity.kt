3614d98c8ee746831a3dd6edf306eda09ba29e9a
Andrea Sanchez <andrea.sanchez0541@alumnos.udg.mx>
2024-11-18 20:28:24 -0600

    Version 2.5 Se crea una ventana nueva para realizar el manejo de las notificaciones e inicios de sesion

--- app/src/main/java/cuvallesl/timeuv_app/MainActivity.kt
+++ app/src/main/java/cuvallesl/timeuv_app/MainActivity.kt
@@ -1,53 +1,61 @@
 package cuvallesl.timeuv_app
-//Importacion de ventanas y procesos
+
+// Importaciones necesarias
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.os.Build
-import cuvallesl.timeuv_app.navigation.NavManager
-//dependencias
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.material3.MaterialTheme
-import androidx.navigation.compose.rememberNavController
 import androidx.compose.material3.Surface
+import androidx.core.content.ContextCompat
+import androidx.navigation.compose.rememberNavController
 import cuvallesl.timeuv_app.fcm.genAndSaveToken
 import cuvallesl.timeuv_app.fcm.getTokenFromPreferences
-
-
+import cuvallesl.timeuv_app.navigation.NavManager
 
 class MainActivity : ComponentActivity() {
-    companion object {
-        const val NOTIFICATION_CHANNEL_ID = "notification_fcm"
+
+    // Registro de permisos dentro de la clase
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // FCM SDK (y tu app) pueden enviar notificaciones
+        } else {
+            // Notificar al usuario que el permiso es necesario
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        // Generamos un token de prueba eliminar! -->
-        genAndSaveToken(this);
-        println(getTokenFromPreferences(this))
-        createNotificationChannel()
 
-        setContent {
-            // Usar un NavController para la navegación
-            val navController = rememberNavController()
-            NavManager(navController = navController) // Configurar el NavManager
-        }
-    }
-
-    private fun createNotificationChannel() {
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        // Crear un canal de notificación si es necesario
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
             val channel = NotificationChannel(
-                NOTIFICATION_CHANNEL_ID,
-                "Notificaciones de FCM",
-                NotificationManager.IMPORTANCE_HIGH
+                "default_channel_id",
+                "Default Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                description = "Canal predeterminado para notificaciones"
+            }
+
+            val notificationManager = ContextCompat.getSystemService(
+                this, NotificationManager::class.java
             )
-            val notificaionManager = getSystemService(NotificationManager::class.java)
-            notificaionManager.createNotificationChannel(channel)
+            notificationManager?.createNotificationChannel(channel)
+        }
+
+        // Configurar el contenido de la actividad
+        setContent {
+            MaterialTheme {
+                Surface {
+                    val navController = rememberNavController()
+                    NavManager(navController = navController)
+                }
+            }
         }
     }
 }
-
